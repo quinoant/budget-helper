3fcf5c0a47e1bbc99033539c989a3001381f6e42
Anthony Quinones <quinoant@gmail.com>
2025-06-10 09:29:18 -0500

    starting point

---
+++ helper/src/test/java/com/budget/helper/HelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
