5b7c95327b9056ddd4411041b0d865d69c80ad30
Anthony Quinones <quinoant@gmail.com>
2025-06-11 14:17:07 -0500

    little test connection

--- frontend-file/frontend/src/App.js
+++ frontend-file/frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [message, setMessage] = useState("Loading...");
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users/102")
+      .then(res => res.text())
+      .then(data => setMessage(data))
+      .catch(err => setMessage("Error loading message"));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>{message}</h1>
     </div>
   );
 }
